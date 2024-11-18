28d651ea392f17a240e6aab2d4302a38b1c38efb
Nafiznayan <abdullah191864@gmail.com>
2024-11-19 02:06:06 +0600

    Adding DOM feature

--- script.js
+++ script.js
@@ -1,76 +1,112 @@
 function getComputerChoice() {
-  const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
+  const computerChoice = ["Rock", "Paper", "Scissors"];
   let randomChoice = Math.floor(Math.random() * 3);
   return computerChoice[randomChoice];
 }
-function getHumanChoice() {
-  let humanChoice = prompt("Enter Rock/Paper/Scissors:");
-  if (
-    humanChoice.toUpperCase() === "ROCK" ||
-    humanChoice.toUpperCase() === "PAPER" ||
-    humanChoice.toUpperCase() === "SCISSORS"
-  ) {
-    return humanChoice.toUpperCase();
-  } else {
-    console.log("Invalid entry, please enter Rock, Paper, or Scissors.");
-    return getHumanChoice();
-  }
-}
+
 let humanScore = 0;
 let computerScore = 0;
-function playRound() {
-  const computerSelection = getComputerChoice();
+let isStart = false;
+const comment = document.querySelector("#comment");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+const h3 = document.querySelector("h3");
+const restart = document.querySelector("#restart");
+let buttons = document.querySelectorAll(".btn");
+let result = document.querySelector("#status");
+restart.addEventListener("click", () => {
+  isStart = true;
+  humanScore = 0;
+  computerScore = 0;
+  computer.innerText = "0";
+  player.innerText = "0";
+  restart.style.display = "none";
+  result.innerText = "";
+  h3.innerText = `Round 1`;
+  buttons.forEach((button) => {
+    button.style.display = "block";
+  });
+});
+let attempt = 0;
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    attempt++;
+    h3.innerText = `Round ${attempt + 1}`;
 
-  const humanSelection = getHumanChoice();
-  if (
-    (computerSelection === "ROCK" && humanSelection === "ROCK") ||
-    (computerSelection === "PAPER" && humanSelection === "PAPER") ||
-    (computerSelection === "SCISSORS" && humanSelection === "SCISSORS")
-  ) {
-    console.log(`Draw!`);
-  } else if (humanSelection === "ROCK" && computerSelection === "PAPER") {
-    console.log(`You Loose! Paper beats Rock`);
-    computerScore++;
-  } else if (humanSelection === "ROCK" && computerSelection === "SCISSORS") {
-    console.log(`You Win! Rock beats Scissors`);
-    humanScore++;
-  } else if (humanSelection === "PAPER" && computerSelection === "SCISSORS") {
-    console.log(`You Loose! Scissors beats Paper`);
-    computerScore++;
-  } else if (humanSelection === "PAPER" && computerSelection === "ROCK") {
-    console.log(`You Win! Paper beats Rock`);
-    humanScore++;
-  } else if (humanSelection === "SCISSORS" && computerSelection === "ROCK") {
-    console.log(`You Loose! Rock beats Scissors`);
-    computerScore++;
-  } else if (humanSelection === "SCISSORS" && computerSelection === "PAPER") {
-    console.log(`You Win! Scissors beats Paper`);
-    humanScore++;
-  } else {
-    console.log("Invalid Entry");
-  }
-}
-for (let i = 1; i <= 5; i++) {
-  playRound();
-}
+    if (attempt <= 10) {
+      const computerSelection = getComputerChoice();
+      let humanSelection = e.target.innerText;
 
-console.log(`Your Score: ${humanScore}`);
-console.log(`Opponents Score: ${computerScore}`);
+      if (
+        (computerSelection === "Rock" && humanSelection === "Rock") ||
+        (computerSelection === "Paper" && humanSelection === "Paper") ||
+        (computerSelection === "Scissors" && humanSelection === "Scissors")
+      ) {
+        comment.innerText = "Draw";
+      } else if (humanSelection === "Rock" && computerSelection === "Paper") {
+        comment.innerText = "You Loose! Paper beats Rock";
+        computerScore++;
+        computer.innerText = `${computerScore}`;
+      } else if (
+        humanSelection === "Rock" &&
+        computerSelection === "Scissors"
+      ) {
+        comment.innerText = "You Win! Rock beats Scissors";
+        humanScore++;
+        player.innerText = `${humanScore}`;
+      } else if (
+        humanSelection === "Paper" &&
+        computerSelection === "Scissors"
+      ) {
+        comment.innerText = "You Loose! Scissors beats Paper";
+        computerScore++;
+        computer.innerText = `${computerScore}`;
+      } else if (humanSelection === "Paper" && computerSelection === "Rock") {
+        comment.innerText = "You Win! Paper beats Rock";
+        humanScore++;
+        player.innerText = `${humanScore}`;
+      } else if (
+        humanSelection === "Scissors" &&
+        computerSelection === "Rock"
+      ) {
+        comment.innerText = "You Loose! Rock beats Scissors";
+        computerScore++;
+        computer.innerText = `${computerScore}`;
+      } else if (
+        humanSelection === "Scissors" &&
+        computerSelection === "Paper"
+      ) {
+        comment.innerText = "You Win! Scissors beats Paper";
+        humanScore++;
+        player.innerText = `${humanScore}`;
+      }
+    }
 
-if (humanScore === computerScore) {
-  console.log("Draw!");
-  alert(`Draw!\nYour Score: ${humanScore}\nOpponents Score: ${computerScore}`);
-}
-if (humanScore > computerScore) {
-  console.log("You Win!");
-  alert(
-    `You Win!\nYour Score: ${humanScore}\nOpponents Score: ${computerScore}`
-  );
-}
+    if (attempt == 10) {
+      // setTimeout(()=>{
 
-if (humanScore < computerScore) {
-  console.log("You Loose!");
-  alert(
-    `You Loose!\nYour Score: ${humanScore}\nOpponents Score: ${computerScore}`
-  );
-}
+      // },1000)
+      isStart = false;
+      restart.style.display = "block";
+      restart.innerText = "Restart";
+      comment.innerText = "";
+
+      h3.innerText = ``;
+      attempt = 0;
+      buttons.forEach((button) => {
+        button.style.display = "none";
+      });
+
+      if (humanScore === computerScore) {
+        result.innerText = "Draw!";
+      }
+      if (humanScore > computerScore) {
+        result.innerText = "You Win!";
+      }
+
+      if (humanScore < computerScore) {
+        result.innerText = "You Lose!";
+      }
+    }
+  });
+});
